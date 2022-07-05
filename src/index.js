abab7e89437d79579bd3c4d96683a54f5ba585a7
aim4dabush <edarasato@gmail.com>
2022-07-05 14:31:24 -0700

    deleted unnessary files, added routes to index.js

--- src/index.js
+++ src/index.js
@@ -4,11 +4,33 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//react router
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+
+//components
+import About from './pages/About';
+import Contact from "./pages/Contact";
+import Footer from "./pages/Footer";
+import MinaDetail from "./pages/MinaDetail";
+import Nav from "./pages/Nav";
+import Shop from "./pages/Shop";
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Router basename={process.env.PUBLIC_URL}>
+    <Routes>
+      <Route exact path={'/'} element={<App/>}>
+        <Route index="/app" element={<App/>} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/footer" element={<Footer />} />
+        <Route path="/minaDetail" element={<MinaDetail />} />
+        <Route path="/nav" element={<Nav />} />
+        <Route path="/shop" element={<Shop />} />
+      </Route>
+    </Routes>
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
