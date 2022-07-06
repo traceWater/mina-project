56b1de0ad8cba621ca9a65a751b4d2382d03ec9f
aim4dabush <edarasato@gmail.com>
2022-07-06 14:01:18 -0700

    finished nav bar

--- src/pages/Nav/Nav.js
+++ src/pages/Nav/Nav.js
@@ -1,8 +1,24 @@
 import React from 'react'
 
+//react router
+import {Link} from 'react-router-dom'
+
+//styles
+import './Nav.css';
+
 function Nav() {
   return (
-    <div>Nav</div>
+    <div className='nav-container'>
+      <div className="nav-bar">
+        <Link to="/"><img src={process.env.PUBLIC_URL + '/Nav_Images/MINA.png'} alt="brand" className='nav-brand' /></Link>
+        <ul>
+          <li><Link to="/shop">Shop</Link></li>
+          <li><Link to="/about" className='drop-down'>About <span>&#9660;</span></Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+          <li><Link to="/">Cart (0)</Link></li>
+        </ul>
+      </div>
+    </div>
   )
 }
 
