769cb20f25fed090cb22e27a0dde263d643ded3e
aim4dabush <edarasato@gmail.com>
2022-07-11 11:18:26 -0700

    finished cermaics section

--- src/pages/Home/Ceramics/Ceramics.js
+++ src/pages/Home/Ceramics/Ceramics.js
@@ -1,5 +1,8 @@
 import React from "react";
 
+//react router
+import { NavLink } from "react-router-dom";
+
 //styles
 import "./Ceramics.css";
 
@@ -10,6 +13,13 @@ function Ceramics() {
         src={process.env.PUBLIC_URL + "/Home_Images/shop_now1.png"}
         alt="ceramics"
       />
+      <div className="ceramics-content-container">
+        <h1 className="ceramics-title">Handmade ceramics</h1>
+        <p className="ceramics-content">Painted clay ceramics made love</p>
+        <NavLink to="/shop" className="ceramics-link">
+          Shop now
+        </NavLink>
+      </div>
     </div>
   );
 }
