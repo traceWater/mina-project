230b47728fd01e5dfb0e26bfd5415c6cc6030933
traceWater <dalecottrell62@gmail.com>
2022-07-11 08:21:40 -0700

    added images and divided link arrays

--- src/pages/Shop/Shop.js
+++ src/pages/Shop/Shop.js
@@ -5,46 +5,96 @@ import "./Shop.css";
 
 export default function Shop() {
   return (
-    <div className="content">
-      <h3 className="centerShop">Shop</h3>
-      <div className="shopTop">
-        {[0].map(p => (
-          <div key={p}>
-            <Link to={`/shopsTop/${p}`}>
-              <img id="centerShop" src={ process.env.PUBLIC_URL + "/ShopImages/dd-d95eByKQX2CoQJ4L.png"} alt="HeroImage"/>
-            </Link>
-          </div>
-        ))}
-      </div>
-      <h3>.</h3>
-      <div className="shops">
-        {[1,2,3].map(p => (
-          <div key={p}>
-            <Link to={`/shops/${p}`}>
-              <img src="https://via.placeholder.com/450x450" alt="." />
-            </Link>
-          </div>
-        ))}
-      </div>
-      <h3>.</h3>
-      <div className="shops">
-        {[4,5,6].map(p => (
-          <div key={p}>
-            <Link to={`/shops/${p}`}>
-              <img src="https://via.placeholder.com/450x450" alt="." />
-            </Link>
-          </div>
-        ))}
-      </div>
-      <h3>.</h3>
-      <div className="shops">
-        {[7,8,9].map(p => (
-          <div key={p}>
-            <Link to={`/shops/${p}`}>
-              <img src="https://via.placeholder.com/450x450" alt="." />
-            </Link>
-          </div>
-        ))}
+    <div>
+       {/* -------hero-image------ */}
+      <h3 className="topCenterShop">Shop</h3>
+        <div className="shopTop">
+          {[0].map(p => (
+            <div key={p}>
+              <Link to={`/shopsTop/${p}`}>
+                <img id="heroShop" src={ process.env.PUBLIC_URL + "/ShopImages/dd-d95eByKQX2CoQJ4L.png"} alt="HeroImage"/>
+              </Link>
+            </div>
+          ))}
+        </div>
+
+        <div className="container">
+
+        {/* ---------row-1-------- */}
+      
+        <div className="shops">
+          {[1].map(p => (
+            <div key={p}>
+              <Link to={`/shops/${p}`}>
+                <img id="row-1" src={ process.env.PUBLIC_URL + "/ShopImages/tanVaseBlueBall.png"} alt="r1"/>
+              </Link>
+            </div>
+          ))}
+          {[2].map(p => (
+            <div key={p}>
+              <Link to={`/shops/${p}`}>
+                <img id="row-1" src={ process.env.PUBLIC_URL + "/ShopImages/greenVaseBluePuck.png"} alt="r1"/>
+              </Link>
+            </div>
+          ))}
+          {[3].map(p => (
+            <div key={p}>
+              <Link to={`/shops/${p}`}>
+                <img id="row-1" src={ process.env.PUBLIC_URL + "/ShopImages/orangeBallGreenStem.png"} alt="r1"/>
+              </Link>
+            </div>
+          ))}
+         </div>
+
+        {/* ---------row-2-------- */}
+         <div className="shops">
+          {[4].map(p => (
+            <div key={p}>
+              <Link to={`/shops/${p}`}>
+                <img id="row-2" src={ process.env.PUBLIC_URL + "/ShopImages/greyVasePurpleRag.png"} alt="r2"/>
+              </Link>
+            </div>
+          ))}
+          {[5].map(p => (
+            <div key={p}>
+              <Link to={`/shops/${p}`}>
+                <img id="row-2" src={ process.env.PUBLIC_URL + "/ShopImages/yellowVaseCactus.png"} alt="r2"/>
+              </Link>
+            </div>
+          ))}
+          {[6].map(p => (
+            <div key={p}>
+              <Link to={`/shops/${p}`}>
+                <img id="row-2" src={ process.env.PUBLIC_URL + "/ShopImages/whiteVaseTwoPucksTanPur.png"} alt="r2"/>
+              </Link>
+            </div>
+          ))}
+         </div>
+
+         {/* ---------row-3-------- */}
+         <div className="shops">
+          {[7].map(p => (
+            <div key={p}>
+              <Link to={`/shops/${p}`}>
+                <img id="row-3" src={ process.env.PUBLIC_URL + "/ShopImages/yellowVaseTwoArches.png"} alt="r3"/>
+              </Link>
+            </div>
+          ))}
+          {[8].map(p => (
+            <div key={p}>
+              <Link to={`/shops/${p}`}>
+                <img id="row-3" src={ process.env.PUBLIC_URL + "/ShopImages/yellowVase-U-Aside.png"} alt="r3"/>
+              </Link>
+            </div>
+          ))}
+          {[9].map(p => (
+            <div key={p}>
+              <Link to={`/shops/${p}`}>
+                <img id="row-3" src={ process.env.PUBLIC_URL + "/ShopImages/tanTribelDisc.png"} alt="r3"/>
+              </Link>
+            </div>
+          ))}
+         </div> 
       </div>
     </div>
   )
