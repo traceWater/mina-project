210545d9ac3b6f9ec247fd40177fcc67c9e743e0
aim4dabush <edarasato@gmail.com>
2022-07-13 14:38:10 -0700

    changed ShopList variable

--- src/pages/Shop/ShopList.js
+++ src/pages/Shop/ShopList.js
@@ -1,68 +1,66 @@
-export const shop = [
+export const Shop = [
+  {
+    id: "0",
+    title: " '' ",
+    image: process.env.PUBLIC_URL + "/ShopImages/TopShopImage.png",
+    price: "",
+  },
 
-    {
-        id: "0",
-        title: " '' ",
-        image: process.env.PUBLIC_URL + "/ShopImages/TopShopImage.png",
-        price: "",
-      },
+  {
+    id: "1",
+    title: "'Balance' Vase",
+    image: process.env.PUBLIC_URL + "/ShopImages/tanVaseBlueBall.png",
+    price: "$79.99",
+  },
+  {
+    id: "2",
+    title: "'Binocular' Vase",
+    image: process.env.PUBLIC_URL + "/ShopImages/greenVaseBluePuck.png",
+    price: "$49.99",
+  },
+  {
+    id: "3",
+    title: "'Bubble' Flowerpot",
+    image: process.env.PUBLIC_URL + "/ShopImages/orangeBallGreenStem.png",
+    price: "$49.99",
+  },
 
-    {
-      id: "1",
-      title: "'Balance' Vase",
-      image: process.env.PUBLIC_URL + "/ShopImages/tanVaseBlueBall.png",
-      price: "$79.99",
-    },
-    {
-      id: "2",
-      title: "'Binocular' Vase",
-      image: process.env.PUBLIC_URL + "/ShopImages/greenVaseBluePuck.png",
-      price: "$49.99",
-    },
-    {
-      id: "3",
-      title: "'Bubble' Flowerpot",
-      image: process.env.PUBLIC_URL + "/ShopImages/orangeBallGreenStem.png",
-      price: "$49.99",
-    },
+  {
+    id: "4",
+    title: "'Bulb' Vase",
+    image: process.env.PUBLIC_URL + "/ShopImages/yellowVaseCactus.png",
+    price: "$39.99",
+  },
+  {
+    id: "5",
+    title: "'Candy' Flowerpot",
+    image: process.env.PUBLIC_URL + "/ShopImages/greyVasePurpleRag.png",
+    price: "$59.99",
+  },
+  {
+    id: "6",
+    title: "'Drop' Vase",
+    image: process.env.PUBLIC_URL + "/ShopImages/whiteVaseTwoPucksTanPur.png",
+    price: "$49.99",
+  },
 
-    {
-        id: "4",
-        title: "'Bulb' Vase",
-        image: process.env.PUBLIC_URL + "/ShopImages/yellowVaseCactus.png",
-        price: "$39.99",
-    },
-    {
-        id: "5",
-        title: "'Candy' Flowerpot",
-        image: process.env.PUBLIC_URL + "/ShopImages/greyVasePurpleRag.png",
-        price: "$59.99",
-      },
-      {
-        id: "6",
-        title: "'Drop' Vase",
-        image: process.env.PUBLIC_URL + "/ShopImages/whiteVaseTwoPucksTanPur.png",
-        price: "$49.99",
-      },
+  {
+    id: "7",
+    title: "'Mango' Flowerpot",
+    image: process.env.PUBLIC_URL + "/ShopImages/yellowVaseTwoArches.png",
+    price: "$49.99",
+  },
+  {
+    id: "8",
+    title: "'Sun' Vase",
+    image: process.env.PUBLIC_URL + "/ShopImages/yellowVase-U-Aside.png",
+    price: "$69.99",
+  },
 
-      {
-        id: "7",
-        title: "'Mango' Flowerpot",
-        image: process.env.PUBLIC_URL + "/ShopImages/yellowVaseTwoArches.png",
-        price: "$49.99",
-      },
-      {
-        id: "8",
-        title: "'Sun' Vase",
-        image: process.env.PUBLIC_URL + "/ShopImages/yellowVase-U-Aside.png",
-        price: "$69.99",
-      },
-      
-      {
-        id: "9",
-        title: "'Wave' Vase",
-        image: process.env.PUBLIC_URL + "/ShopImages/tanTribelDisc.png",
-        price: "$69.99",
-      },
-  ];
-  
+  {
+    id: "9",
+    title: "'Wave' Vase",
+    image: process.env.PUBLIC_URL + "/ShopImages/tanTribelDisc.png",
+    price: "$69.99",
+  },
+];
